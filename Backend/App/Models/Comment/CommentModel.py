ec6e6dc0640ab215584c0beb5b2d8c05e4a07943
SahilBanwala <sahilbanwala2002@gmail.com>
2024-08-13 09:43:52 +0530

    Add username in Comments

--- Backend/App/Models/Comment/CommentModel.py
+++ Backend/App/Models/Comment/CommentModel.py
@@ -6,6 +6,7 @@ import uuid
 class Comment(db.Model):
     uid = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     content = db.Column(db.Text, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
     user_uid = db.Column(UUID(as_uuid=True), db.ForeignKey('user.uid', ondelete='CASCADE'), nullable=False)
     post_uid = db.Column(UUID(as_uuid=True), db.ForeignKey('post.uid', ondelete='CASCADE'), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
